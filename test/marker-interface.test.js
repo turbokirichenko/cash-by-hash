a95d04d76b7253f7eb3c3f4ff3ac2deb2911ebc2
Andrew <turbokirichenko@outlook.com>
2022-04-02 05:15:52 +0200

    update tests

--- test/marker-interface.test.js
+++ test/marker-interface.test.js
@@ -1,22 +1,24 @@
 //
 const testing = require('./eq');
 const bs58check = require('bs58check');
+const base64 = require('base-64');
 const op = require('./../src/components/marker-interface');
 //
 it('testing default marker', function() {
 	const password = 'e0f78514441ff57fe0546c1591396f847a1d78ca09a30e9396a8865848535092';
 	const seckey = 'ff0b43b2832905149a9bd05b9b3e57a75b10d54fe4f221c7cbeec1d4df3d7800';
 	const result = op.GenMarker(Buffer.from(password, 'hex'), Buffer.from(seckey, 'hex'));
-	testing.eq_string(result, '2uT5PdZdCaamDKGgBCQazUcg5WFWwhWe3h67knVpr2RRfpx2PVofwJh6yN');
+	testing.eq_string(result, 'choajqzZGINixw0Sj65TeGlnmYytiYJq3WGTCEYTn47IAaz8Isw=');
 });
 it('extract network', function () {
-	const dec_marker = bs58check.decode('2uT5PdZdCaamDKGgBCQazUcg5WFWwhWe3h67knVpr2RRfpx2PVofwJh6yN').toString('hex');
+	const test_marker = 'choajqzZGINixw0Sj65TeGlnmYytiYJq3WGTCEYTn47IAaz8Isw=';
+	const dec_marker = Buffer.from(test_marker, 'base64').toString('hex');
 	const result = op.ExtractNetwork(dec_marker.substr(dec_marker.length - 4, 4));
 	testing.eq_string(result, 'BTCTEST');
 });
 it('extract partition', function () {
-	const marker = '2uT5PdZdCaamDKGgBCQazUcg5WFWwhWe3h67knVpr2RRfpx2PVofwJh6yN';
-	const dec_marker = bs58check.decode(marker).toString('hex');
+	const marker = 'choajqzZGINixw0Sj65TeGlnmYytiYJq3WGTCEYTn47IAaz8Isw=';
+	const dec_marker = Buffer.from(marker, 'base64').toString('hex');
 	const body = 'acd9188362c70d128fae53786967998cad89826add61930846139f8ec801acfc22cc';
 	const result = op.ExtractMarkerPartition(marker);
 	testing.eq_string(result.body, body);
--- test/registry-interface.test.js
+++ test/registry-interface.test.js
@@ -4,20 +4,20 @@ const md5 = require('md5');
 const bs58check = require('bs58check');
 const op = require('./../src/components/registry-interface');
 it('testing nametag', function () {
-	const marker = '2uT5PdZdCaamDKGgBCQazUcg5WFWwhWe3h67knVpr2RRfpx2PVofwJh6yN';
+	const marker = 'choajqzZGINixw0Sj65TeGlnmYytiYJq3WGTCEYTn47IAaz8Isw=';
 	const password_buf = Buffer.from('e0f78514441ff57fe0546c1591396f847a1d78ca09a30e9396a8865848535092','hex');
 	const pwd_hash = md5(password_buf);
 	const result = op.GenNameTag('test', marker, pwd_hash);
-	const nametag = '<test************06cebb>';
+	const nametag = '<test************0ec83e>';
 	testing.eq(result, nametag);
 });
 it('control registry line', function () {
-	const marker = '2uT5PdZdCaamDKGgBCQazUcg5WFWwhWe3h67knVpr2RRfpx2PVofwJh6yN';
+	const marker = 'choajqzZGINixw0Sj65TeGlnmYytiYJq3WGTCEYTn47IAaz8Isw=';
 	const password_buf = Buffer.from('e0f78514441ff57fe0546c1591396f847a1d78ca09a30e9396a8865848535092','hex');
 	const pwd_hash = md5(password_buf);
-	const nametag = '<test************06cebb>2uT5PdZdCaamDKGgBCQazUcg5WFWwhWe3h67knVpr2RRfpx2PVofwJh6yN';
+	const nametag = '<test************0ec83e>choajqzZGINixw0Sj65TeGlnmYytiYJq3WGTCEYTn47IAaz8Isw=';
 	const result_one = op.ExtractMarker(nametag, pwd_hash);
-	testing.eq(result_one, marker);
+	testing.eq(result_one.marker, marker);
 	const result_two = op.ExtractMarker(nametag, 'test1234test1234');
-	testing.eq(result_two, false);
+	testing.eq(result_two.res, false);
 });
